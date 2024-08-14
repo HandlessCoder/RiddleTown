62697d5268e30253db9391d374d1a4f791217de8
Asknaby <nohelyn.ucv@gmail.com>
2024-08-14 19:11:49 -0400

    test de usuario sin errores

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -6,28 +6,36 @@ from mainapp.models import User
 class CustomUserManager (TestCase):
 
     def setUp(self):
+
         self.user= User.objects.create_user(
             email='django@gmail.com',
             password='123456',
             nickname='prueba',
             country='country',
             estate='estate',
-            address='address'
+            address='address',
+            is_staff= False,
+            is_superuser= False,
+            is_active= True
         )
 
-    def test_superuser_creation(self):
         self.superUser = User.objects.create_superuser(
             email ='django2@gmail.com',
             nickname ='pruebaSuper',
-            password ='789101'
-    )
-        
-    def test_staff_user_creation(self):
+            password ='789101',
+            is_superuser= True
+        )
+
         self.staffUser = User.objects.create_user(
             email ='django3@gmail.com',
             nickname ='pruebaStaff',
             password ='2345678',
             is_staff =True
-    )
-        
+        )
+
+        def test_UserType(self):
+            
+            assert self.is_superuser == True, "Es un Super Usuario"
+            assert self.is_Staff == True, "Es un Usuario Staff"
+            
         
